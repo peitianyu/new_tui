78c3d1f0d28b9b3211a2b0ef7acf236a4de3978d
tiantian <1216582461@qq.com>
2025-08-02 17:33:15 +0800

    update: 更新测试

--- src/test/tests/test_render.c
+++ src/test/tests/test_render.c
@@ -8,15 +8,13 @@
 TEST(render, test) {
     canvas_init(80, 24);
     canvas_clear();
-    printf("\e[?25h");
     style_t st = { .fg = 15, .bg = 4, .text = 1, .rect = 1, .border = 1, .border_st = 2 };
     canvas_draw((rect_t){5, 5, 20, 3}, "光标在下一格 ▶", st);
 
-    st = (style_t){ .fg = 15, .bg = 3, .text = 1, .bold = 1, .strike = 1 };
-    canvas_draw((rect_t){5, 3, 20, 1}, "光标在下一格 ▶", st);
+    st = (style_t){ .fg = 0, .bg = 3, .text = 1, .bold = 1 };
+    canvas_draw((rect_t){5, 3, 20, 1}, "──────────────", st);
 
     canvas_flush();                 // 会输出带光标的帧
-    canvas_cursor_move(15, 5, 5);   // 把真实光标挪到文字后面
     getchar();                      // 按回车结束
     canvas_free();
     printf("\e[?25h");
